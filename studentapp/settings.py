3e7d1d181c0afaeef84414422af7e5de98c9ad4d
rameshd07 <ramesh.2004dpm@gmail.com>
2025-09-03 20:29:54 +0530

    Prepare for Render: gunicorn, whitenoise, env settings

--- studentapp/settings.py
+++ studentapp/settings.py
@@ -26,13 +26,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY = "django-insecure-&=di=1t%5sy(rkka4@y%)(m4yhe0y5s1yl&3*#e=)5)lhk#(_^"
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-insecure-key")
-DEBUG = True
+# DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["*"]  # for local; restrict in prod
+# ALLOWED_HOSTS = ["*"]  # for local; restrict in prod
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+
 
 
 # Application definition
